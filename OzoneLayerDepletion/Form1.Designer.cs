256e4c04cfbfc3034d3c4207c5f7096745e0aa29
TheGingerNinjaC <vanderbergchane@gmail.com>
2018-09-02 14:56:58 +0200

    Initial commit

---
+++ OzoneLayerDepletion/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace OzoneLayerDepletion
+{
+    partial class ozoneDepletionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(12, 46);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(266, 160);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.Location = new System.Drawing.Point(12, 9);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(266, 34);
+            this.descriptionLabel.TabIndex = 1;
+            this.descriptionLabel.Text = "Click below to see by how much DU the ozone layer shrinks every 10 years!";
+            this.descriptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(50, 216);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 35);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show Depletion";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(163, 216);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 35);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // ozoneDepletionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 276);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "ozoneDepletionForm";
+            this.Text = "Ozone Layer Depletion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ OzoneLayerDepletion/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OzoneLayerDepletion
+{
+    public partial class ozoneDepletionForm : Form
+    {
+        public ozoneDepletionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Constants
+            const double RATE = 0.04;
+            const int DECADE = 10;
+            const int START_YEAR = 2018;
+            const int END_YEAR = 2118;
+
+            // Variables
+            double du = 300;
+            double shrink;
+            int year;
+
+            // Display table of DU depletion
+            for (year = START_YEAR; year <= END_YEAR; year += DECADE)
+            {
+                // Calculate the amount it shrinks by
+                shrink = du * RATE;
+
+                // Calculate the new value
+                du -= shrink;
+
+                // Display the output
+                outputListBox.Items.Add("From " + year + " the ozone layer shrinks by " + shrink.ToString("f") + ".");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
